1ca0e23ea0e96fa591ef207a84e0247d87ea1378
Patrick <saxbeck.patrick@gmail.com>
2021-05-04 12:47:27 +0200

    Cleanup
    Files cleaned up, branches should be alright and working now

--- app/src/main/java/com/example/youbank/fragments/KeypadFragment.kt
+++ app/src/main/java/com/example/youbank/fragments/KeypadFragment.kt
@@ -1,6 +1,5 @@
-package com.example.youbank
+package com.example.youbank.fragments
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import com.example.youbank.R
 
 class KeypadFragment : Fragment() {
 
