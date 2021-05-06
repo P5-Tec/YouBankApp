0118b3f3a977522aa4871b0e367915723017eacd
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-05-06 08:59:47 +0200

    Refactor transaction to recyclerview
    Added transaction recyclerview

---
+++ app/src/main/java/com/example/youbank/fragments/MyTransactionListRecyclerViewAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.youbank.fragments
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.youbank.R
+
+import com.example.youbank.fragments.dummy.DummyContent.DummyItem
+
+/**
+ * [RecyclerView.Adapter] that can display a [DummyItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyTransactionListRecyclerViewAdapter(
+    private val values: List<DummyItem>
+) : RecyclerView.Adapter<MyTransactionListRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.transaction_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.txtTransactionAmount)
+
+    }
+}
