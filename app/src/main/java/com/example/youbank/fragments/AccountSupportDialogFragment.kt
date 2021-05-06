6c4dd4f5e23b55b16dedafd663f53139dc396351
Patrick <saxbeck.patrick@gmail.com>
2021-05-06 08:26:55 +0200

    account_support_fragment
    Added AccountSupportDialogFragment
    Added AccountSupportDialogTestFragment

---
+++ app/src/main/java/com/example/youbank/fragments/AccountSupportDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.example.youbank.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.youbank.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AccountSupportDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_support_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            AccountSupportDialogFragment().apply {
+            }
+    }
+}
