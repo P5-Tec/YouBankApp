817c0add59112158472f31cddfeebdd337484519
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-05-11 13:17:22 +0200

    Feature updates
    miscellaneous

--- app/src/main/java/com/example/youbank/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/youbank/fragments/AccountFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import com.example.youbank.R
 
 class AccountFragment : Fragment() {
@@ -21,6 +25,14 @@ class AccountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ConstraintLayout>(R.id.clayout).setOnClickListener {
+            findNavController().navigate(R.id.action_homeScreenFragment_to_accountOverviewFragment)
+        }
     }
 
 }
