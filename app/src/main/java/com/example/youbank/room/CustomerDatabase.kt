91d75c7f6d7765a3c3913c32d736af298e70484e
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-06-29 09:00:36 +0200

    removed converters class

--- app/src/main/java/com/example/youbank/room/CustomerDatabase.kt
+++ app/src/main/java/com/example/youbank/room/CustomerDatabase.kt
@@ -15,7 +15,6 @@ import com.example.youbank.room.daos.TransactionDao
 import kotlinx.coroutines.CoroutineScope
 
 @Database(entities = [Customer::class, Account::class, Card::class, Transaction::class], version = 1, exportSchema = false)
-//@TypeConverters(Converters::class)
 abstract class CustomerDatabase: RoomDatabase() {
 
     abstract fun customerDao(): CustomerDao
