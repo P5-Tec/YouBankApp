c0007ef7a9056d7b7d3d42d83b29a5412977da1d
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-07-08 09:32:17 +0200

    redundant log

--- app/src/main/java/com/example/youbank/fragments/listFragments/AccountListFragment.kt
+++ app/src/main/java/com/example/youbank/fragments/listFragments/AccountListFragment.kt
@@ -1,7 +1,6 @@
 package com.example.youbank.fragments.listFragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,7 +40,6 @@ class AccountListFragment: Fragment() {
                 accountAdapter = AccountAdapter()
                 adapter = accountAdapter
                 model.allAccounts.observe(viewLifecycleOwner, { it?.let { accountAdapter.submitList(it) } })
-                Log.i("accounts", adapter?.itemCount.toString())
             }
         }
         return view
