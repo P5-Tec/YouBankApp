902be7a2dd1519f004256697c74705ffe4b70d31
Martin Jensen <martin.jensen1996@yahoo.dk>
2021-07-08 09:34:10 +0200

    keeping these in case we need to take a look at it again

--- app/src/main/java/com/example/youbank/retrofit/repo/RetrofitCustomerRepository.kt
+++ app/src/main/java/com/example/youbank/retrofit/repo/RetrofitCustomerRepository.kt
@@ -1,20 +1,18 @@
 package com.example.youbank.retrofit.repo
 
-import com.example.youbank.models.Customer
 import com.example.youbank.retrofit.ApiService
 import com.example.youbank.retrofit.CustomerService
-import com.example.youbank.retrofit.client.RetrofitClient
 
 class RetrofitCustomerRepository {
 
     var client: CustomerService = ApiService.buildService(CustomerService::class.java)
 
     //var client2: CustomerService = ApiService.buildService(CustomerService::class.java)
-    var client3: CustomerService = RetrofitClient.retrofit.create(CustomerService::class.java)
+    //var client3: CustomerService = RetrofitClient.retrofit.create(CustomerService::class.java)
 
     suspend fun getCustomerById(cId: Int) = client.getCustomerById(cId)
 
-    suspend fun getCustomerById2(id: Int): Customer {
-        return client3.getCustomerById(id)
-    }
+    //suspend fun getCustomerById2(id: Int): Customer {
+    //    return client3.getCustomerById(id)
+    //}
 }
